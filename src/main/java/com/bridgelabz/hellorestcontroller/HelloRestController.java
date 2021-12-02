cd08bc45f5e62b9e081c4a975c16856e3c3cbc6b
Siraj <sirajkhanc2305@gmail.com>
2021-12-02 19:24:15 +0530

    Created: Created HelloWorld application in spring boot

---
+++ src/main/java/com/bridgelabz/hellorestcontroller/HelloRestController.java
@@ -0,0 +1,40 @@
+package com.bridgelabz.hellorestcontroller;
+
+import com.bridgelabz.hellorestcontroller.dto.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloRestController {
+
+    @GetMapping(value = {"/hello", "/"})
+    public String usingGetMethod() {
+        return "Hello from BridgeLabz";
+    }
+
+    @RequestMapping(value = {"/sayHello"})
+    public String sayHello(
+            @RequestParam(value = "name") String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/sayName/{name}")
+    public String sayHello1(
+            @PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @PostMapping(value = "/user")
+    public User byUsingDto(
+            @RequestBody User user) {
+        return user;
+    }
+
+    @PutMapping(value = "/sayMyName/{firstName}")
+    public String sayHelloFirstAndLastName(
+            @PathVariable(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName
+    ) {
+        return "Hello " + firstName + " " + lastName;
+    }
+}
